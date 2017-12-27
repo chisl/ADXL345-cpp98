f0ef94ccf83105e3a01e464d0f641c6594cfdf6b
chisl <info@chisl.io>
2017-12-27 18:10:49 +0100

    Initial commit

---
+++ ADXL345.cpp
@@ -0,0 +1,13 @@
+/*
+ * name:        ADXL345
+ * description: Small, thin, ultralow power, 3-axis accelerometer with high resolution (13-bit) measurement at up to ±16 g.
+ * manuf:       Analog Devices Inc.
+ * version:     0.1
+ * url:         http://www.analog.com/media/en/technical-documentation/data-sheets/ADXL345.pdf
+ * date:        2016-08-01
+ * author       https://chisl.io/
+ * file:        ADXL345.cpp
+ */
+
+#include "ADXL345.hpp"
+
